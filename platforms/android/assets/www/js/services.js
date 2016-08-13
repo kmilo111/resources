3837b465df68dfa4844982e6ad5d36dafa2d9259
ssssss <xxxxxx@xxxx.com>
2016-08-13 12:19:08 -0500

    resources
    resources

---
+++ platforms/android/assets/www/js/services.js
@@ -0,0 +1,8 @@
+angular.module('app.services', ['ngResource'])
+
+.service('productService', ['$resource', function($resource){
+	 this.item_list = $resource('http://private-bcbaa-productapp.apiary-mock.com/products/list');
+
+ 	 this.item_detail = $resource('http://private-bcbaa-productapp.apiary-mock.com/products/detail/:id');
+
+}]);
